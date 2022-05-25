8fbf9fde3f11d9c06ec5d2b1be0fb5a5f4469b93
João Gabriel Camargo de Freitas <83920066+JoaoFreitasGab@users.noreply.github.com>
2022-05-25 10:37:25 -0300

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -53,6 +53,26 @@ for(var i = 0; i<btn.length; i++){
         var btnVal  = this.innerHTML,
         inputVal = input.innerHTML;
         //console.log(btnVal);
+
+        switch(btnVal){
+            case "c":
+                input.innerHTML = "";
+                break;
+                case"=":
+                var equacao = inputVal;
+
+                if(equacao){
+                    try{
+                        input.innerHTML = eval(equacao);
+                    } catch (e){
+                        alert ("Erro na Expressão")
+                    }
+                }
+                break;
+                default:
+                    input.innerHTML += btnVal;
+                    break;
+        }
     })
 
     }
